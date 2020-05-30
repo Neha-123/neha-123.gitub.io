d5fda053d5eb472382f14e74c8252b35239dd4fa
unknown <nehasingh1252@gmail.com>
2020-05-30 18:41:38 +0530

    Updates

---
+++ precache-manifest.1ceb86d547c8327eab6530d8b8a4b3ed.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c5241da45f726db0920b05430745666a",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "c016c2724503137269b8",
+    "url": "/robofriends/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "0cffa5c8d7b749bc8ddf",
+    "url": "/robofriends/static/css/main.7efa85e9.chunk.css"
+  },
+  {
+    "revision": "c016c2724503137269b8",
+    "url": "/robofriends/static/js/2.88b08eb1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/robofriends/static/js/2.88b08eb1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0cffa5c8d7b749bc8ddf",
+    "url": "/robofriends/static/js/main.4616ac9f.chunk.js"
+  },
+  {
+    "revision": "99557a09ab68caec6407",
+    "url": "/robofriends/static/js/runtime-main.229ccf3b.js"
+  },
+  {
+    "revision": "95db6f04af88eea5abb63a1e4e0d67b8",
+    "url": "/robofriends/static/media/sega-webfont.95db6f04.woff2"
+  }
+]);
